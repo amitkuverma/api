c20cf4f60d1c94cfb7353686772ee9c803fd7379
AMIT VERMA <amit.verma@softobotics.com>
2024-10-11 08:24:49 +0530

    code adde

--- src/config/config.ts
+++ src/config/config.ts
@@ -14,46 +14,38 @@ function getEnvVar(key: string): string {
   return value;
 }
 
-const config: Record<Env, {
-  db: {
-    username: string;
-    password: string;
-    database: string;
-    host: string;
-    dialect: string;
-    port:number;
-  };
-  corsOrigin: string;
-  swaggerUrl: string;
+const config: Record<Env, { 
+  db: { 
+    username: string; 
+    password: string; 
+    database: string; 
+    host: string; 
+    dialect: string; 
+  }; 
+  corsOrigin: string; 
+  swaggerUrl: string; 
 }> = {
   development: {
     db: {
-      username:  'sql12732393',
-      password: 'tC6TIGpLNs',
-      database:  'sql12732393',
-      host: 'sql12.freesqldatabase.com',
-      dialect: 'mysql',
-      port: 3306
-      // username: getEnvVar('DB_USER'),       // Get from env
-      // password: getEnvVar('DB_PASSWORD'),   // Get from env
-      // database: getEnvVar('DB_NAME'),       // Get from env
-      // host: getEnvVar('DB_HOST'),           // Get from env
-      // dialect: getEnvVar('DB_DIALECT'),     // Get from env
+      username: getEnvVar('DB_USER'),       // Get from env
+      password: '',   // Get from env
+      database: getEnvVar('DB_NAME'),       // Get from env
+      host: getEnvVar('DB_HOST'),           // Get from env
+      dialect: getEnvVar('DB_DIALECT'),     // Get from env
     },
     corsOrigin: '*',   // Get from env
-    swaggerUrl: 'https://gyanvidigital.xyz/gorakha-coin-api/api-docs',   // Get from env
+    swaggerUrl: getEnvVar('SWAGGER_URL'),   // Get from env
   },
   production: {
     db: {
-      username:  'sql12732393',
-      password: 'tC6TIGpLNs',
-      database:  'sql12732393',
-      host: 'sql12.freesqldatabase.com',
-      dialect: 'mysql',
-      port: 3306
+      username: getEnvVar('DB_USER'),       // Get from env
+      password: getEnvVar('DB_PASSWORD'),   // Get from env
+      database: getEnvVar('DB_NAME'),       // Get from env
+      host: getEnvVar('DB_HOST'),           // Get from env
+      dialect: getEnvVar('DB_DIALECT'),     // Get from env
     },
-    corsOrigin: '*',   // Get from env
-    swaggerUrl: 'https://gyanvidigital.xyz/gorakha-coin-api/api-docs',   // Get from env
+    corsOrigin: getEnvVar('CORS_ORIGIN'),   // Get from env
+    swaggerUrl: getEnvVar('SWAGGER_URL'),   // Get from env
   },
 };
 
